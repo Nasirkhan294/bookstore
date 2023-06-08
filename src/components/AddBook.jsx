f63ee0f6df778a50b06ee10f538ced665c4dd718
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-06-08 19:18:06 +0500

    Added h2 test in add book component

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,11 +1,14 @@
 import styles from './styles/AddBook.module.css';
 
 const AddBook = () => (
-  <div className={styles.newBook}>
-    <input type="text" placeholder="title" className={styles.title} />
-    <input type="text" placeholder="author" className={styles.author} />
-    <button type="submit" className={styles.btn}>Add Book</button>
-  </div>
+  <>
+    <h2>Add new book</h2>
+    <div className={styles.newBook}>
+      <input type="text" placeholder="title" className={styles.title} />
+      <input type="text" placeholder="author" className={styles.author} />
+      <button type="submit" className={styles.btn}>Add Book</button>
+    </div>
+  </>
 );
 
 export default AddBook;
