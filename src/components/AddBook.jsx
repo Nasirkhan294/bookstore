0d4e169ad5ed04611b7734877313b0d7fee33407
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-06-08 19:20:01 +0500

    Updated code with minor fixes

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -3,11 +3,11 @@ import styles from './styles/AddBook.module.css';
 const AddBook = () => (
   <>
     <h2>Add new book</h2>
-    <div className={styles.newBook}>
+    <form className={styles.newBook}>
       <input type="text" placeholder="title" className={styles.title} />
       <input type="text" placeholder="author" className={styles.author} />
       <button type="submit" className={styles.btn}>Add Book</button>
-    </div>
+    </form>
   </>
 );
 
