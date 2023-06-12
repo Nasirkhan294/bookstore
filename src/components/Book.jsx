4dc606497554fb5e678869999b15e3594e1b72d5
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-06-12 20:22:46 +0500

    Add redux reducer and sote in to the project structure

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,15 +2,13 @@ import PropTypes from 'prop-types';
 import styles from './styles/Book.module.css';
 
 const Book = ({ title, author }) => (
-  <>
-    <div className={styles.book}>
-      <div className={styles.content}>
-        <p>{title}</p>
-        <p>{author}</p>
-        <button type="button" className={styles.btn}>Remove</button>
-      </div>
+  <div className={styles.book}>
+    <div className={styles.content}>
+      <p>{title}</p>
+      <p>{author}</p>
+      <button type="button" className={styles.btn}>Remove</button>
     </div>
-  </>
+  </div>
 );
 
 Book.propTypes = {
