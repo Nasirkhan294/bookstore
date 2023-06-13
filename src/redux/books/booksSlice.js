7f87c679e83827eefe2972aa51241659b170a466
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2023-06-13 18:17:59 +0500

    Use redux in react components

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,16 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const initialState = {
+  books: [
+    {
+      itemId: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      itemId: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      itemId: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
+};
+
 const booksSlice = createSlice({
   name: 'books',
-  initialState: {
-    books: [],
-  },
+  initialState,
   reducers: {
     addBook: (state, action) => {
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      state.books = state.books.filter((book) => book.id !== action.payload);
+      state.books = state.books.filter((book) => book.itemId !== action.payload);
     },
   },
 });
